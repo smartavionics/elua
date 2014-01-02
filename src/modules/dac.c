0f48d9dcfecde26a3dc78e7f054968ab6d0c89db
Mark Burton <markb@smartavionics.com>
2014-01-02 17:40:11 +0000

    Misc changes based on initial feedback.
    Renamed dac.init() to dac.setup().
    
    dac.putsamples() can now take the sample data as either a string or an
    array of integer values.
    
    dac.putsamples() loses the bytes_per_sample, offset and num_samples args.

--- src/modules/dac.c
+++ src/modules/dac.c
@@ -13,30 +13,32 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Lua: init( id, [bits_per_sample, [options]] )
-static int dac_init(lua_State *L) {
+static unsigned bytes_per_sample[NUM_DAC];
+
+// Lua: setup( id, [bits_per_sample] )
+static int dac_setup(lua_State *L) {
   
   unsigned id = luaL_checkinteger(L, 1);
   MOD_CHECK_ID(dac, id);
 
   unsigned bits_per_sample = luaL_optinteger(L, 2, 8);
 
-  unsigned options = luaL_optinteger(L, 3, 0);
-
-  int result = platform_dac_init(id, bits_per_sample, options);
+  int result = platform_dac_init(id, bits_per_sample, 0);
   if(result != DAC_INIT_OK)
     luaL_error(L, "DAC initialisation failed (%d)", result);
+
+  bytes_per_sample[id] = (bits_per_sample + 7) / 8;
 
   return 0;
 }
 
-// Lua: putsample( id, [val] )
+// Lua: putsample( id, val )
 static int dac_putsample(lua_State  *L) {
   
   unsigned id = luaL_checkinteger(L, 1);
   MOD_CHECK_ID(dac, id);
 
-  u16 val = luaL_optinteger(L, 2, 0);
+  u16 val = luaL_checkinteger(L, 2);
 
   platform_dac_put_sample(1 << id, &val);
 
@@ -50,7 +52,6 @@ static struct {
   volatile unsigned num_samples;  // total number of samples to output
   volatile unsigned next_out;     // offset of next byte in sample buffer to be output
   unsigned next_in;               // offset of next byte in sample buffer to be filled
-  unsigned bytes_per_sample;      // number of bytes for each sample (per channel)
   unsigned stride;                // amount next_out is incremented by when a sample is consumed
   unsigned bias;                  // add this value to sample data before sending to the DAC
   unsigned dac_id;                // the id of the DAC
@@ -65,7 +66,7 @@ static void dac_timer_int_handler(elua_int_resnum resnum) {
       available += dac_state.sample_buffer_size;
     if(available >= dac_state.stride) {
       u16 sample;
-      if(dac_state.bytes_per_sample == 1) {
+      if(bytes_per_sample[dac_state.dac_id] == 1) {
         sample = dac_state.bias + dac_state.sample_buffer[dac_state.next_out];
       }
       else {
@@ -85,16 +86,14 @@ static void dac_timer_int_handler(elua_int_resnum resnum) {
   }
 }
 
-// Lua: putsamples( id, samples, rate, [timer_id, [bytes_per_sample, [bias, [offset, [num_samples]]]]] )
+// Lua: putsamples( id, samples, rate, [timer_id, [bias]] )
 static int dac_putsamples(lua_State *L) {
 
   dac_state.dac_id = luaL_checkinteger(L, 1);
   MOD_CHECK_ID(dac, dac_state.dac_id);
 
-  size_t byte_count;
-  const char *samples = luaL_checklstring(L, 2, &byte_count);
-  if(!samples)
-    return 0;
+  if(lua_type(L, 2) != LUA_TSTRING && lua_type(L, 2) != LUA_TTABLE)
+    return luaL_error(L, "samples must either be a string or an array of integers");
 
   unsigned rate = luaL_checkinteger(L, 3);
   if(rate == 0)
@@ -107,19 +106,10 @@ static int dac_putsamples(lua_State *L) {
   MOD_CHECK_TIMER(dac_state.timer_id);
   MOD_CHECK_RES_ID(dac, dac_state.dac_id, timer, dac_state.timer_id);
 
-  dac_state.bytes_per_sample = luaL_optinteger(L, 5, 1);
-  dac_state.stride = dac_state.bytes_per_sample;
-  dac_state.bias = luaL_optinteger(L, 6, 0);
-  unsigned offset = luaL_optinteger(L, 7, 0);
-  if(offset > byte_count)
-    luaL_error(L, "offset must be less than length of samples string");
-  dac_state.num_samples = luaL_optinteger(L, 8, (byte_count - offset) / dac_state.bytes_per_sample);
-  if(dac_state.num_samples * dac_state.bytes_per_sample + offset > byte_count)
-    luaL_error(L, "num_samples too large");
+  dac_state.stride = bytes_per_sample[dac_state.dac_id];
+  dac_state.bias = luaL_optinteger(L, 5, 0);
 
-  dac_state.sample_buffer = (char *)samples + offset;
-  dac_state.sample_buffer_size = dac_state.num_samples * dac_state.bytes_per_sample;
-  dac_state.next_in = dac_state.sample_buffer_size;
+  dac_state.next_in = 0;
   dac_state.next_out = 0;
 
   // install our timer interrupt handler
@@ -129,8 +119,45 @@ static int dac_putsamples(lua_State *L) {
   if(result != PLATFORM_TIMER_INT_OK)
     return luaL_error(L, "Failed to start DAC timer (%d)", result);
 
-  while(dac_state.num_samples != 0) {
-    // twiddle thumbs
+  if(lua_type(L, 2) == LUA_TTABLE) {
+    int num_vals_in_table = lua_objlen(L, 2);
+    dac_state.num_samples = num_vals_in_table;
+    char small_buffer[16];
+    dac_state.sample_buffer = small_buffer;
+    dac_state.sample_buffer_size = sizeof(small_buffer);
+    // copy values from table into circular buffer
+    int n = 1;
+    while(dac_state.num_samples != 0) {
+      if(n <= num_vals_in_table) {
+        int space = dac_state.next_out - dac_state.next_in;
+        if(space < 0)
+          space += dac_state.sample_buffer_size;
+        if(space > dac_state.stride) {
+          lua_rawgeti(L, 2, n++);
+          unsigned val = luaL_checkinteger(L, -1);
+          lua_pop(L, 1);
+          int i;
+          for(i = 0; i < dac_state.stride; ++i) {
+            // FIXME - cope with big endian format?
+            dac_state.sample_buffer[dac_state.next_in] = val;
+            val >>= 8;
+            if(++dac_state.next_in >= dac_state.sample_buffer_size)
+              dac_state.next_in -= dac_state.sample_buffer_size;
+          }
+        }
+      }
+    }
+  }
+  else {
+    // LUA_TSTRING
+    size_t byte_count = 0;
+    dac_state.sample_buffer = (char *)luaL_checklstring(L, 2, &byte_count);
+    dac_state.sample_buffer_size = byte_count;
+    dac_state.num_samples = dac_state.sample_buffer_size / dac_state.stride;
+    dac_state.next_in = dac_state.sample_buffer_size;
+    while(dac_state.num_samples != 0) {
+      // twiddle thumbs
+    }
   }
 
   // stop timer interrupts
@@ -165,7 +192,7 @@ static int dac_playwavfile(lua_State *L) {
   if(wavfile == NULL)
     return luaL_error(L, "Can't open %s for reading", wavfilename);
 
-  uint8_t header[24];
+  u8 header[24];
   if(fread(header, 1, 12, wavfile) != 12) {
     fclose(wavfile);
     return luaL_error(L, "Failed to read from %s", wavfilename);
@@ -187,17 +214,17 @@ static int dac_playwavfile(lua_State *L) {
     return luaL_error(L, "%s header does not contain format chunk", wavfilename);
   }
 
-  uint32_t chunk_size = header[4] + (header[5] << 8) + (header[6] << 16) + (header[7] << 24);
-  uint32_t compression_code = header[8] + (header[9] << 8);
-  uint32_t num_channels = header[10] + (header[11] << 8);
-  uint32_t sample_rate = header[12] + (header[13] << 8) + (header[14] << 16) + (header[15] << 24);
-  uint32_t block_align = header[20] + (header[21] << 8);
+  unsigned chunk_size = header[4] + (header[5] << 8) + (header[6] << 16) + (header[7] << 24);
+  unsigned compression_code = header[8] + (header[9] << 8);
+  unsigned num_channels = header[10] + (header[11] << 8);
+  unsigned sample_rate = header[12] + (header[13] << 8) + (header[14] << 16) + (header[15] << 24);
+  unsigned block_align = header[20] + (header[21] << 8);
 
   if(chunk_size > 16)
     fread(header, 1, chunk_size - 16, wavfile);
 
   dac_state.stride = block_align;
-  dac_state.bytes_per_sample = block_align / num_channels;
+  unsigned bytes_per_sample = block_align / num_channels;
 
   if(compression_code != 1) {
     // punt if data isn't uncompressed PCM
@@ -205,7 +232,7 @@ static int dac_playwavfile(lua_State *L) {
     return luaL_error(L, "%s does not contain uncompressed PCM data", wavfilename);
   }
 
-  if(dac_state.bytes_per_sample > 2) {
+  if(bytes_per_sample > 2) {
     // punt if data isn't 8 or 16 bit
     fclose(wavfile);
     return luaL_error(L, "%s does not contain 8 or 16 bit PCM data", wavfilename);
@@ -221,17 +248,17 @@ static int dac_playwavfile(lua_State *L) {
     return luaL_error(L, "%s does not contain a data chunk", wavfilename);
   }
 
-  int result = platform_dac_init(dac_state.dac_id, dac_state.bytes_per_sample * 8, 0);
+  int result = platform_dac_init(dac_state.dac_id, bytes_per_sample * 8, 0);
   if(result != DAC_INIT_OK) {
     fclose(wavfile);
     return luaL_error(L, "DAC initialisation failed (%d)", result);
   }
 
-  uint32_t data_len = header[4] + (header[5] << 8) + (header[6] << 16) + (header[7] << 24);
+  unsigned data_len = header[4] + (header[5] << 8) + (header[6] << 16) + (header[7] << 24);
 
   dac_state.num_samples = data_len / dac_state.stride;
-  
-  dac_state.bias = dac_state.bytes_per_sample == 1 ? 0xffffff80 : 0xffff8000;
+
+  dac_state.bias = bytes_per_sample == 1 ? 0xffffff80 : 0xffff8000;
 
   dac_state.sample_buffer = malloc(dac_state.sample_buffer_size);
   if(!dac_state.sample_buffer) {
@@ -252,6 +279,7 @@ static int dac_playwavfile(lua_State *L) {
     return luaL_error(L, "Failed to start DAC timer (%d)", result);
   }
 
+  // loop until all samples have been output
   while(dac_state.num_samples != 0) {
     // as buffer drains, refill it a byte at a time
     int space = dac_state.next_out - dac_state.next_in;
@@ -283,7 +311,7 @@ static int dac_playwavfile(lua_State *L) {
 
 // Module function map
 const LUA_REG_TYPE dac_map[] = { 
-  { LSTRKEY("init"),  LFUNCVAL(dac_init) },
+  { LSTRKEY("setup"),  LFUNCVAL(dac_setup) },
   { LSTRKEY("putsample"),  LFUNCVAL(dac_putsample) },
   { LSTRKEY("putsamples"),  LFUNCVAL(dac_putsamples) },
   { LSTRKEY("playwavfile"),  LFUNCVAL(dac_playwavfile) },
